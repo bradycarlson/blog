5767267031e922f628c436778083a3129585cfd7
Brady Carlson <bradycarlson@Bradys-MacBook-Pro.local>
2016-01-29 17:34:42 -0700

    added commenting functionality - (no validation on comments at this time)

---
+++ app/models/comment.rb
@@ -0,0 +1,7 @@
+class Comment < ActiveRecord::Base
+  belongs_to :article
+
+#  validates :commenter, presence: true, length: { minimum: 3 }
+
+#  validates :comment, presence: true, length: { minimum: 3 }
+end
